6855a2e08f5be3aa6fd4f61c03ff4fce387727da
juan jose <juanvizzari135gmail.com>
2025-06-25 00:24:20 -0300

    creacion del proyecto con vue

---
+++ src/router/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import PostForm from '../components/PostForm.vue';
+import PostList from '../views/PostList.vue';
+
+const routes = [
+
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
