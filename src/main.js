a06b16bf3c4950f1cd20b1cbcc896db118edb38d
juan jose <juanvizzari135gmail.com>
2025-07-03 18:53:39 -0300

    vista del login

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
 
 import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
